e6a211c15009134b5a62a6bee3e56f8b8a8b5bee
abhishekporwal-836 <abhishekporwal836@gmail.com>
2024-09-04 12:51:49 +0530

    manifests created

--- flask-mongodb-app/venv/app.py
+++ flask-mongodb-app/venv/app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-client = MongoClient(os.environ.get("MONGODB_URI", "mongodb://localhost:27017/"))
+client = MongoClient(os.environ.get("MONGODB_URI", "mongodb+srv://abhishekporwal836:<password>@flask-app-buyogo.5yoi7.mongodb.net/?retryWrites=true&w=majority&appName=flask-app-BUYOGO"))
 
 db = client.flask_db
 
